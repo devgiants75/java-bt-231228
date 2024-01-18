34bfdbe05a6ec2c232b1781d39f6f100b4fcd3c8
devgiants75 <devgiants75@naver.com>
2024-01-18 11:27:46 +0900

    20240118

--- JAVA_bt1228/src/k_Chapter10/Z_Practice01.java
+++ JAVA_bt1228/src/k_Chapter10/Z_Practice01.java
@@ -1,7 +1,18 @@
 package k_Chapter10;
 
+// 모바일 애플리케이션이라는 추상 클래스를 선언(MobileApp)
+// : '실행'이라는 추상 메서드를 선언(execute)
+// : '앱 설명'이라는 일반 메서드를 구현(appInfo)
+
+// '게임 애플리케이션'과 (GameApp)
+// '사회 네트워킹 애플리케이션'이라는 클래스를 생성 (SocialApp)
+// : 모바일 애플리케이션 추상 클래스를 상속받는 자식클래스
+// : 각 클래스에서 '실행' 메서드를 구현
+// : '게임 애플리케이션'에서만 일반 메서드 재정의
+
 public class Z_Practice01 {
 	public static void main(String[] args) {
-		
+		// 메인메서드에서 GameApp, SocialApp 인스턴스 생성
+		// 각각의 메서드 실행
 	}
 }
--- JAVA_bt1228/src/k_Chapter10/Z_Practice02.java
+++ JAVA_bt1228/src/k_Chapter10/Z_Practice02.java
@@ -1,7 +1,24 @@
 package k_Chapter10;
 
+//Fruit 인터페이스를 정의
+//인터페이스에는 color라는 추상 메서드와 
+
+//describe라는 디폴트 메서드
+//디폴트 메서드"This is a fruit of color: "
+
+//printType라는 정적 메서드
+//"This is a type of fruit."
+
+// 인터페이스 구현
+// : 다음으로, Apple과 Orange 클래스를 만들어 Fruit 인터페이스를 구현
+// : 각 클래스는 color 메서드를 오버라이드하여 색상을 반환
+
 public class Z_Practice02 {
 	public static void main(String[] args) {
+		// 디폴트 메서드 활용
+		// describe 메서드는 Fruit 인터페이스의 디폴트 메서드
+		// Apple과 Orange 객체에서 호출 가능
 		
+		// 정적 메서드 활용
 	}
 }
